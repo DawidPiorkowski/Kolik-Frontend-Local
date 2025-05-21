10c89541e61cd518178d268091b5fc5494cab288
Dren Krasniqi <drenkrasniqi29@hotmail.com>
2025-05-21 21:44:45 +0200

    Updated Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 // src/App.tsx
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import Home                  from './pages/Home'
-import Login                 from './pages/Login'
-import Register              from './pages/Register'
-import VerifyEmail           from './pages/VerifyEmail'
-import PasswordResetRequest  from './pages/PasswordResetRequest'
-import PasswordResetConfirm  from './pages/PasswordResetConfirm'
-import MfaSetup              from './pages/MfaSetup'
-import MfaLogin              from './pages/MfaLogin'
-import Products              from './pages/Products'
-import ProductDetail         from './pages/ProductDetail'
-import ShoppingList          from './pages/ShoppingList'
-import ProtectedTest         from './pages/ProtectedTest'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import VerifyEmail from './pages/VerifyEmail';
+import PasswordResetRequest from './pages/PasswordResetRequest';
+import PasswordResetConfirm from './pages/PasswordResetConfirm';
+import MfaSetup from './pages/MfaSetup';
+import MfaLogin from './pages/MfaLogin';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import ShoppingList from './pages/ShoppingList';
+import ProtectedTest from './pages/ProtectedTest';
 
-// Profile 
-import AccountSettings       from './pages/AccountSettings'
-import ChangePassword        from './pages/ChangePassword'
-import ChangeEmail           from './pages/ChangeEmail'
-import ConfirmEmail          from './pages/ConfirmEmail'
+// Profile
+import AccountSettings from './pages/AccountSettings';
+import ChangePassword from './pages/ChangePassword';
+import ChangeEmail from './pages/ChangeEmail';
+import ConfirmEmail from './pages/ConfirmEmail';
 
-import ProtectedRoute from './components/ProtectedRoute'
-import Navbar         from './components/Navbar'
-import Layout         from './components/Layout'
+// Contact
+import Contact from './pages/contact'; // 👈 NEW Contact page
+
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
+import Layout from './components/Layout';
 
 export default function App() {
   return (
@@ -31,32 +34,49 @@ export default function App() {
       <Navbar />
       <Layout>
         <Routes>
-          {/* public */}
-          <Route path="/"                  element={<Home />} />
-          <Route path="/register"          element={<Register />} />
+          {/* ── PUBLIC ROUTES ── */}
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/verify-email/:token" element={<VerifyEmail />} />
-          <Route path="/password-reset"    element={<PasswordResetRequest />} />
+          <Route path="/password-reset" element={<PasswordResetRequest />} />
           <Route path="/password-reset/confirm" element={<PasswordResetConfirm />} />
-          <Route path="/login"             element={<Login />} />
-          <Route path="/mfa-setup"         element={<MfaSetup />} />
-          <Route path="/mfa-login"         element={<MfaLogin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mfa-setup" element={<MfaSetup />} />
+          <Route path="/mfa-login" element={<MfaLogin />} />
+          <Route path="/contact" element={<Contact />} /> {/* 👈 ADDED ROUTE */}
 
-          {/* protected */}
+          {/* ── PROTECTED ROUTES ── */}
           <Route
             path="/products"
-            element={<ProtectedRoute><Products /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <Products />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/products/:id"
-            element={<ProtectedRoute><ProductDetail /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <ProductDetail />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/shopping-list"
-            element={<ProtectedRoute><ShoppingList /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <ShoppingList />
+              </ProtectedRoute>
+            }
           />
           <Route
             path="/protected"
-            element={<ProtectedRoute><ProtectedTest /></ProtectedRoute>}
+            element={
+              <ProtectedRoute>
+                <ProtectedTest />
+              </ProtectedRoute>
+            }
           />
 
           {/* ── ACCOUNT SETTINGS FLOW ── */}
@@ -84,7 +104,6 @@ export default function App() {
               </ProtectedRoute>
             }
           />
-          {/* this one reads ?token=… from the URL, no path‐param needed */}
           <Route
             path="/account/confirm-email-change/:token"
             element={
@@ -96,5 +115,5 @@ export default function App() {
         </Routes>
       </Layout>
     </>
-  )
+  );
 }
